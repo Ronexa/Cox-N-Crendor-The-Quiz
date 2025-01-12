6dcd6b1562cc9c42d9e2672490ce182e97b973ce
Ronexa <lazu90@gmail.com>
2025-01-13 00:23:49 +0200

    recordings

--- questions.js
+++ questions.js
@@ -1,15 +1,59 @@
 const questions = [
     {
-        question: "Who said this quote: 'I love coffee!'?",
+        question: "Who said this quote: 'Wow, you certainly don't look fat in those jeans'?",
         correctAnswer: "Jesse",
         source: "Cox n Crendor Podcast Episode 1",
-        audio: "sounds/audio.mp3"
+        audio: "sounds/episode1x1.mp3"
     },
     {
-        question: "Who said this quote: 'I hate mornings!'?",
+        question: "Who said this quote: 'If you want a woman to like you just go up to her and be like: hey you almost dont look fat in those jeans'?",
         correctAnswer: "Crendor",
-        source: "Cox n Crendor Podcast Episode 2"
-    }
+        source: "Cox n Crendor Podcast Episode 1",
+        audio: "sounds/episode1x2.mp3"
+    },  
+    {
+        question: "Who said this quote: 'Idiots dont eat treebark well unless they deepfry it that might be delicious'?",
+        correctAnswer: "Jesse",
+        source: "Cox n Crendor Podcast Episode 1",
+        audio: "sounds/episode1x3.mp3"
+    },
+    {
+        question: "Who said this quote: 'Your death is really boring'?",
+        correctAnswer: "Jesse",
+        source: "Cox n Crendor Podcast Episode 1",
+        audio: "sounds/episode1x4.mp3"
+    },
+    {
+        question: "Who said this quote: 'Imagination is for nerds'?",
+        correctAnswer: "Jesse",
+        source: "Cox n Crendor Podcast Episode 1",
+        audio: "sounds/episode1x5.mp3"
+    },
+    {
+        question: "Who said this quote: 'I'm not a doctor, but I'm pretty sure that's not how you do it'?",
+        correctAnswer: "Crendor",
+        source: "Cox n Crendor Podcast Episode 2",
+        audio: "sounds/episode2x4.mp3"
+    },
+    {
+        question: "Who said this quote: 'I'm not a doctor, but I'm pretty sure that's not how you do it'?",
+        correctAnswer: "Jesse",
+        source: "Cox n Crendor Podcast Episode 2",
+        audio: "sounds/episode2x5.mp3"
+    },
+    {
+        question: "Who said this quote: 'I'm not a doctor, but I'm pretty sure that's not how you do it'?",
+        correctAnswer: "Crendor",
+        source: "Cox n Crendor Podcast Episode 2",
+        audio: "sounds/episode2x6.mp3"
+    },
+    {
+        question: "Who said this quote: 'I'm not a doctor, but I'm pretty sure that's not how you do it'?",
+        correctAnswer: "Jesse",
+        source: "Cox n Crendor Podcast Episode 2",
+        audio: "sounds/episode2x7.mp3"
+    },
+    
     // Add more questions here
 ];
 
